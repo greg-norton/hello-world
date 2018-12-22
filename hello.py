33e4b26a5a22fe07e3d487dca494817b87af3b28
greg-norton <46059853+greg-norton@users.noreply.github.com>
2018-12-21 16:48:26 -0800

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,10 @@
+'''
+Filename: hello.py
+Date: 12/21/18
+
+This is a hello world program written in Python which I am using in my attempt to learn how to use Git.
+
+'''
+
+print('Hello, World!')
+
